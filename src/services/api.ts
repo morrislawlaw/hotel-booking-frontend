35ac8db11b3852f638ff3fd17abb49527284faca
morrislawlaw <morrislawlaw@gmail.com>
2026-05-25 18:16:21 +0800

    real domain name update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://124.244.80.213:8001/api',   // ← Change to your actual port
+  baseURL: 'https://www.488865.xyz/api',   // ← Change to your actual port
   headers: {
     'Content-Type': 'application/json',
   },
