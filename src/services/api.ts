796f2ca06f4aa04148879fe37199d4e507c3f1c0
morrislawlaw <morrislawlaw@gmail.com>
2026-05-29 15:57:05 +0800

    use Azure API platform

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
+// const api = axios.create({
+//   baseURL: 'https://www.488865.xyz/api',   // ← Cloudflare tunnel of local IIS port 
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// })
+
 const api = axios.create({
-  baseURL: 'https://www.488865.xyz/api',   // ← Change to your actual port
+  baseURL: 'https://hotelapi-f5bdebbke6eceyfg.southeastasia-01.azurewebsites.net',   // ← Azure App Service as API 
   headers: {
     'Content-Type': 'application/json',
   },
